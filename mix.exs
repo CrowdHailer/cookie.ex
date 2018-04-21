5fa8e217435d97080214e50f98ca2cf0ef0b4d2d
CrowdHailer <peterhsaxton@gmail.com>
2018-04-21 19:08:19 +0100

    fix github link for package

--- mix.exs
+++ mix.exs
@@ -33,6 +33,6 @@ defmodule Cookie.Mixfile do
     [
      maintainers: ["Peter Saxton"],
      licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/CrowdHailer/raxx/tree/master/cookie"}]
+     links: %{"GitHub" => "https://github.com/CrowdHailer/raxx/tree/master/cookie.ex"}]
   end
 end
